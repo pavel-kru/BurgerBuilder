f5bd06141aab25cbd2c79d5ca32640f3db08256b
pavel-kru <pavlusha.kruglik@gmail.com>
2021-05-24 11:08:42 +0300

    styles were updated

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -5,7 +5,6 @@ import Order from "../../components/Order/Order";
 import axios from "../../axios-orders";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import * as actions from "../../store/actions/order";
-import Spinner from "../../components/UI/Spinner/Spinner";
 
 const Orders = (props) => {
   const { onFetchOrders } = props;
